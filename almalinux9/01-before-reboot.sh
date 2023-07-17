4489919cd7cc0192bf27bff1a04359d098e3ea50
Kamil Aronowski <kamil.aronowski@yahoo.com>
2023-07-17 15:43:36 +0200

    Let's try pesign-113-21 on AlmaLinux 9

--- almalinux9/01-before-reboot.sh
+++ almalinux9/01-before-reboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-sudo dnf install -y grubby pesign softhsm 
+sudo dnf install -y grubby https://repo.almalinux.org/vault/9.0/AppStream/x86_64/os/Packages/pesign-113-21.el9.x86_64.rpm softhsm 
 sudo usermod -a -G pesign vagrant
 sudo bash -c "echo 'vagrant' >> /etc/pesign/users"
 sudo grubby --update-kernel ALL --args selinux=0
