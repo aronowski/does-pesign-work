57fdcc6ecab389a34aadc2f450990a80e0d166d0
Kamil Aronowski <kamil.aronowski@yahoo.com>
2023-07-27 09:50:08 +0200

    Verify this on a system with hardware-accelerated virtualization

--- fedora38/01-before-reboot.sh
+++ fedora38/01-before-reboot.sh
@@ -1,8 +1,24 @@
 #!/bin/bash -x
 
-sudo dnf install -y grubby pesign softhsm
+sudo dnf install -y grubby rpmdevtools softhsm
+
+# What happens when cms_common has debug mode enabled?
+rpmdev-setuptree
+dnf download --source pesign-116-2
+rpmdev-extract -C ~/rpmbuild/SOURCES/ pesign-116-2.fc38.src.rpm
+cd ~/rpmbuild/SOURCES/
+mv */* .
+cp /vagrant/force_debug.patch .
+echo "Patch1001: force_debug.patch" >> pesign.patches
+sudo dnf install -y autoconf automake bison device-mapper-devel flex fuse-devel gettext-devel help2man ncurses-devel rpm-devel squashfs-tools texinfo
+rpmbuild -D 'dist .fc38.aronowski' -ba *.spec
+sudo dnf install -y ~/rpmbuild/RPMS/x86_64/*.rpm
+
 sudo usermod -a -G pesign vagrant
 sudo bash -c "echo 'vagrant' >> /etc/pesign/users"
+sudo /usr/libexec/pesign/pesign-authorize
+
 sudo grubby --update-kernel ALL --args selinux=0
 
 softhsm2-util --init-token --label HSM --so-pin Secret.123 --pin Secret.123 --free
+sudo chmod -R 777 /var/lib/softhsm/
--- fedora38/02-after-reboot.sh
+++ fedora38/02-after-reboot.sh
@@ -4,6 +4,7 @@ sudo systemctl start pesign
 export PESIGN_TOKEN_PIN="Secret.123"
 modutil -dbdir /etc/pki/pesign -list
 
-sudo chmod -R 777 /run/pesign /var/run/pesign
+pesign-client --debug -u -t HSM #fail for unknown reason?
 
-pesign-client -u -t HSM #fail for unknown reason?
+# Let's see a (hopefully) meaningful error message
+#sudo journalctl --no-pager --grep=cms_common --unit=pesign.service --output=cat --lines=1
