6d38db3168324b53c9552fe35471a60cd67540a9
Kamil Aronowski <kamil.aronowski@yahoo.com>
2023-07-31 13:07:40 +0200

    Some fixes for practical Vagrant laboratory tests

--- fedora38/01-before-reboot.sh
+++ fedora38/01-before-reboot.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -x
 
-sudo dnf install -y grubby rpmdevtools softhsm
+sudo dnf install -y grubby rpmdevtools softhsm autoconf automake bison device-mapper-devel flex fuse-devel gettext-devel help2man ncurses-devel rpm-devel squashfs-tools texinfo efivar-devel gcc git libuuid-devel make mandoc nspr-devel nss-devel nss-tools python3-rpm-macros
 
 # What happens when cms_common has debug mode enabled?
 rpmdev-setuptree
-dnf download --source pesign-116-2
+dnf download --source pesign-116-2.fc38
 rpmdev-extract -C ~/rpmbuild/SOURCES/ pesign-116-2.fc38.src.rpm
 cd ~/rpmbuild/SOURCES/
 mv */* .
 cp /vagrant/force_debug.patch .
 echo "Patch1001: force_debug.patch" >> pesign.patches
-sudo dnf install -y autoconf automake bison device-mapper-devel flex fuse-devel gettext-devel help2man ncurses-devel rpm-devel squashfs-tools texinfo
 rpmbuild -D 'dist .fc38.aronowski' -ba *.spec
 sudo dnf -C install -y ~/rpmbuild/RPMS/x86_64/*.rpm
 
