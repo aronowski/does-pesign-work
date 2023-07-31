ab22aa978b623eeb75d8e58448d6102cff0db93a
Kamil Aronowski <kamil.aronowski@yahoo.com>
2023-07-31 16:14:24 +0200

    setenforce 0

--- fedora38/01.sh
+++ fedora38/01.sh
@@ -6,7 +6,7 @@ sudo usermod -a -G pesign vagrant
 sudo bash -c "echo 'vagrant' >> /etc/pesign/users"
 sudo /usr/libexec/pesign/pesign-authorize
 
-sudo grubby --update-kernel ALL --args selinux=0
+sudo setenforce 0 ; sudo grubby --update-kernel ALL --args selinux=0
 
 softhsm2-util --init-token --label HSM --so-pin Secret.123 --pin Secret.123 --free
 sudo chmod -R 777 /var/lib/softhsm/
