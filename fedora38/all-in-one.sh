fec889368b88112291f27b19a95e1c8873405fc6
Kamil Aronowski <kamil.aronowski@yahoo.com>
2023-08-16 09:09:30 +0200

    Try to be noninteractive this time x5

--- fedora38/all-in-one.sh
+++ fedora38/all-in-one.sh
@@ -13,9 +13,10 @@ modutil -dbdir /etc/pki/pesign -list
 
 # .p12 stuff
 git clone https://github.com/rhboot/shim.git --depth 1
-cd shim
+pushd shim
 ./make-certs example
 pk12util -k <(echo "${PESIGN_TOKEN_PIN}") -w /dev/zero -i example.p12 -d /etc/pki/pesign -h HSM
+popd
 
 # permission fixes to be applied only after the .p12 import
 sudo chmod -R 777 /var/lib/softhsm/
@@ -29,6 +30,7 @@ pesign-client --is-unlocked --token HSM
 # let's try signing shimx64.efi
 dnf download shim
 rpmdev-extract shim-x64-*.x86_64.rpm
-cd /home/vagrant/shim-x64-*.x86_64/boot/efi/EFI/fedora
+pushd /home/vagrant/shim-x64-*.x86_64/boot/efi/EFI/fedora
 pesign --remove-signature --signature-number=0 --in=shimx64.efi --out=shimx64.efi.unsigned
 pesign-client -t HSM -c example -i ~/shimx64.efi.unsigned -o ~/shimx64.efi.signed -s 
+popd
