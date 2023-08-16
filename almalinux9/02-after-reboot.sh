fea345e38c0e02c693431ff62fa18746cd6bae22
Kamil Aronowski <kamil.aronowski@yahoo.com>
2023-08-16 12:11:43 +0200

    Let's try moving the working Fedora 38 config to AlmaLinux 9

--- almalinux9/02-after-reboot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-sudo systemctl start pesign
-export PESIGN_TOKEN_PIN="Secret.123"
-modutil -dbdir /etc/pki/pesign -list
-
-sudo chmod -R 777 /run/pesign /var/run/pesign
-
-pesign-client -u -t HSM #fail for unknown reason?
---
+++ almalinux9/all-in-one.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -xe
+
+sudo dnf install -y git grubby openssl pesign rpmdevtools softhsm
+
+sudo usermod -a -G pesign vagrant
+#sudo bash -c "echo 'vagrant' >> /etc/pesign/users" # unused?
+
+sudo setenforce 0 ; sudo grubby --update-kernel ALL --args selinux=0
+
+export PESIGN_TOKEN_PIN="Secret.123"
+softhsm2-util --init-token --label HSM --so-pin ${PESIGN_TOKEN_PIN} --pin ${PESIGN_TOKEN_PIN} --free
+modutil -dbdir /etc/pki/pesign -list
+
+# .p12 stuff
+git clone https://github.com/rhboot/shim.git --depth 1
+pushd shim
+./make-certs example
+pk12util -k <(echo "${PESIGN_TOKEN_PIN}") -w /dev/zero -i example.p12 -d /etc/pki/pesign -h HSM
+popd
+
+# permission fixes to be applied only after the .p12 import
+sudo chmod -R 777 /var/lib/softhsm/
+sudo /usr/libexec/pesign/pesign-authorize
+
+sudo dnf reinstall -y pesign && sudo systemctl start pesign
+
+pesign-client -u -t HSM #fail for unknown reason?
+pesign-client --is-unlocked --token HSM
+
+# let's try signing shimx64.efi
+dnf download shim
+rpmdev-extract shim-x64-*.x86_64.rpm
+pushd /home/vagrant/shim-x64-*.x86_64/boot/efi/EFI/fedora
+pesign --remove-signature --signature-number=0 --in=./shimx64.efi --out=./shimx64.efi.unsigned
+pesign-client -t HSM -c example -i ./shimx64.efi.unsigned -o ./shimx64.efi.signed -s && echo "Signing works!"
+popd
